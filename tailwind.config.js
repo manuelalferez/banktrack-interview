fcc3b72971c1ccde9f742d864fe840cff43cd06d
Manuel Alférez <manuelalferezruiz@gmail.com>
2022-06-27 20:24:26 +0200

    feat: installed material-tailwind & new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = {
+module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
     colors: {
       ...colors,
-      mygray: "#FEFAF4",
+      mypeach: "#FEFAF4",
+      mygray: "#D3D3D3",
+      myblue: "#2E16B9",
+      mylightblue: "#5B34DE",
     },
   },
   plugins: [],
-};
+});
