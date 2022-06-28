03935f7253d80ee5ec60e93d5ca96fdc9cffc885
Manuel Alférez <manuelalferezruiz@gmail.com>
2022-06-28 18:32:54 +0200

    feat: created Error page & integrated react router in the project

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Error from "./Error";
+import Layout from "./components/Layout";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/error" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
